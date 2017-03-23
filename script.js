76523f9f221611a5478f9ec77ab2bfc74929d68f
Phuc Le <le.phuc.h@gmail.com>
2017-03-23 15:30:23 -0700

    Add functions for changing break and work time lengths.

--- script.js
+++ script.js
@@ -2,14 +2,57 @@
 // User Story: I can reset the clock for my next pomodoro.
 // User Story: I can customize the length of each pomodoro.
 
-var getInitialIntervals = () {
+// press - or + to subtract or add time values
+// when - or +
+
+var getInitialIntervals = () =>  {
 
 }
 
-var addTime = () {
+var addTimeChangeDetection = () => {
+  // select all time buttons
+  var subtractBreakTime = document.getElementById("subtractBreakTime");
+  var addBreakTime = document.getElementById("addBreakTime");
+  var subtractWorkTime = document.getElementById("subtractWorkTime");
+  var addWorkTime = document.getElementById("addWorkTime");
+
+  var addTimeButtons = [addBreakTime, addWorkTime];
+  var subtractTimeButtons = [subtractBreakTime, subtractWorkTime];
+
+  subtractTimeButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      subtractTime(button);
+    });
+  });
+  addTimeButtons.map(button => {
+    return button.addEventListener("click", () => {
+      addTime(button);
+    });
+  });
 
 }
 
-var subtractTime = () {
-  
+var subtractTime = (button) => {
+    // going two siblings over to get to the element, skipping the stuff between
+  var timeValue = button.nextSibling.nextSibling.innerText;
+  timeValue = parseInt(timeValue);
+  timeValue--;
+  button.nextSibling.nextSibling.innerText = timeValue;
 }
+
+var addTime = (button) => {
+  // going two siblings over to get to the element, skipping the stuff between
+  var timeValue = button.previousSibling.previousSibling.innerText;
+  timeValue = parseInt(timeValue);
+  timeValue++;
+  button.previousSibling.previousSibling.innerText = timeValue;
+}
+
+
+var updateTimer = () => {
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  addTimeChangeDetection();
+});
