0afe015b26627867b150e674d6e2f215d8b35b19
Phuc Le <le.phuc.h@gmail.com>
2017-03-24 15:16:06 -0700

    Add a basic working countdown function.

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// User Story: I can start a 25 minute pomodoro, and the timer will go off once 25 minutes has elapsed.
+// --- User Story: I can start a 25 minute pomodoro, and the timer will go off once 25 minutes has elapsed.
 // User Story: I can reset the clock for my next pomodoro.
-// User Story: I can customize the length of each pomodoro.
-
-// press - or + to subtract or add time values
-// when - or +
+// ---User Story: I can customize the length of each pomodoro.
 
 // TimeController class
 // Initial time value, subtract method, add method
@@ -37,43 +34,74 @@ class TimeController {
 
 var addTimeChangeDetection = () => {
   // select all time buttons
-  var subtractBreakTime = document.getElementById("subtractBreakTime");
-  var addBreakTime = document.getElementById("addBreakTime");
-  var subtractWorkTime = document.getElementById("subtractWorkTime");
-  var addWorkTime = document.getElementById("addWorkTime");
+  var subtractBreakTime = document.getElementById('subtractBreakTime');
+  var addBreakTime = document.getElementById('addBreakTime');
+  var subtractWorkTime = document.getElementById('subtractWorkTime');
+  var addWorkTime = document.getElementById('addWorkTime');
 
   // select session length elements
   var breakTimer = document.getElementById('breakTimer');
   var workTimer = document.getElementById('workTimer');
 
+  var clock = document.getElementById('clock');
+
   //create time controller objects
   var breakController = new TimeController(breakTimer);
   var workController = new TimeController(workTimer);
 
-  subtractBreakTime.addEventListener("click", () => {
+  subtractBreakTime.addEventListener('click', () => {
     breakController.subtractTime();
   });
 
-  addBreakTime.addEventListener("click", () => {
+  addBreakTime.addEventListener('click', () => {
     breakController.addTime();
   });
 
-  subtractWorkTime.addEventListener("click", () => {
+  subtractWorkTime.addEventListener('click', () => {
     workController.subtractTime();
   });
 
-  addWorkTime.addEventListener("click", () => {
+  addWorkTime.addEventListener('click', () => {
     workController.addTime();
+  });
+
+  clock.addEventListener('click', () => {
+    countdown();
   });
 
 }
 
-var countdown = (minutes) => {
-  //set initial time to minutes,
+var countdown = (mode) => {
+  var countdownTimer = document.getElementById('countdown');
+  var minutes;
   var seconds = 0;
+  if (mode === 'break') {
+    var breakLength = document.getElementById('breakTimer').innerText;
+    minutes = parseInt(breakLength);
+  } else {
+    minutes = parseInt(countdownTimer.innerText);
+  }
 
+  function updateTimer() {
+    if (seconds < 10) {
+        countdownTimer.innerText = minutes + ':' + '0' + seconds.toString();
+    } else {
+      countdownTimer.innerText = minutes + ':' + seconds;
+    }
+
+    if (minutes === 0 && seconds === 0) {
+      mode === 'break' ? countdown('work') : countdown('break');
+    }
+    else if (seconds === 0) {
+      seconds = 59;
+      minutes--;
+    } else {
+      seconds--;
+    }
+  }
+  window.setInterval(updateTimer, 1000)
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   addTimeChangeDetection();
 });
